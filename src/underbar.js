05a21f9a193e13b8dfc09d3b80b219b85adf7fbb
Irfan Baqui <irfan.baqui@gmail.com>
2015-02-26 01:33:03 -0600

    Implemented functions till .default

--- src/underbar.js
+++ src/underbar.js
@@ -238,11 +238,22 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var x=1; x < arguments.length; x++) {
+      for (var j in arguments[x]) 
+        obj[j] = arguments[x][j];
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var x=1; x < arguments.length; x++) {
+      for (var j in arguments[x]) 
+        if (j in obj) ; 
+          else obj[j] = arguments[x][j];
+    }
+    return obj;
   };
 
 
